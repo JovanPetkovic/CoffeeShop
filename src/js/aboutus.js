1eb4fe62d2450ea08c1b13766eb96da5060a81db
SoniXx1337 <jovan.sonixx123@gmail.com>
2017-06-28 00:10:08 +0200

    Fixed critiacl issues with parallax and added optimization

--- src/js/aboutus.js
+++ src/js/aboutus.js
@@ -12,26 +12,42 @@ $(function(){
         var parallaxImage = {}
         parallaxImage.element = $(this)
         parallaxImage.offsetTop = Math.floor(parallaxImage.element.offset().top);
-        parallaxImage.offsetBottom = Math.floor(parallaxImage.element.height() + parallaxImage.offsetTop);
-        parallaxImage.speed = parallaxImage.element.attr('data-speed');
+        parallaxImage.height1 = parallaxImage.element.height();
+        parallaxImage.offsetBottom = Math.floor(parallaxImage.height1 + parallaxImage.offsetTop);
+        parallaxImage.speed = parseInt(parallaxImage.element.attr('data-speed'));
         parallaxImages.push(parallaxImage);
     })
     var wScroll = Math.floor($(window).scrollTop());
-    console.log(parallaxImages[0].speed);
-    function scrollHandler(object){
-        object.element.css({
-            'transform': 'translate(0px,-'+ wScroll/object.speed+'%)'
-        })
+    function scrollHandler(object, c){
+        object.element.animate({
+            top: c+'='+object.speed
+        },1);
+        if(c==='+'){
+            object.offsetTop += object.speed;
+            object.offsetBottom += object.speed;
+        }
+        else {
+            object.offsetTop -= object.speed;
+            object.offsetBottom -= object.speed;
+        }
     }
+    var lastScrollTop = 0;
+    var c='';
     $(window).scroll(function(){
         wScroll = Math.floor($(this).scrollTop());
+        if(lastScrollTop<$(window).scrollTop()){
+            c='-'
+        }
+        else {
+            c='+'
+        }
         window.requestAnimationFrame(function(){
-            for(var i=0;i<parallaxImages.length;i++){
-                if((wScroll+wHeight>=parallaxImages[i].offsetTop)&&(wScroll+wHeight<=parallaxImages[i].offsetBottom)){
-                    console.log('heey');
-                    scrollHandler(parallaxImages[i]);
+            for(var i=0;i<=parallaxImages.length-1;i++){
+                if((wScroll+wHeight>=parallaxImages[i].offsetTop)&&(wScroll<=parallaxImages[i].offsetBottom)){
+                    scrollHandler(parallaxImages[i],c);
                 }
             }
         })
+        lastScrollTop = $(window).scrollTop();
     })
 })
